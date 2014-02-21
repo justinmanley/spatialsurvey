ac254dd16f8bfe6124cd1d0dc22ddf27ddf2edcd
Justin Manley <manleyjster@gmail.com>
2014-02-21 11:00:03 -0600

    Update save.php so that it will work on server.

--- save.php
+++ save.php
@@ -1,28 +1,26 @@
-<?php 
+<?php
 session_start();
 
 require_once('../../config.php');
 
-if ( !$dbport )
-	$conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
-else 
-	$conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $dbport);
+$conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $dbport);
 if ( !$conn ) { die ('Could not connect: ' . mysqli_error($conn)); }
 
+$user_id = $_SERVER['persistent-id'];
 $jsondata = mysqli_escape_string($conn, $_SESSION['path-data']);
-
 $kmlstring = require_once('kml_template.php');
 $kml = mysqli_escape_string($conn, $kmlstring);
+
 $sql = <<<sqlstring
-INSERT INTO paths ( id, kml_string, json_string, time_submitted ) VALUES ( DEFAULT, "$kml", "$jsondata", NOW() )
+INSERT INTO paths ( id, user_id, kml_string, json_string, time_submitted ) VALUES ( DEFAULT, "$user_id", "$kml", "$jsondata", NOW() )
 sqlstring;
 
 mysqli_select_db($conn, $dbname);
 
 $retval = mysqli_query($conn, $sql);
 
-	if ( !$retval ) { die('Could not enter data: ' . mysqli_error($conn)); }
-	else { successful_database_save(); }
+        if ( !$retval ) { die('Could not enter data: ' . mysqli_error($conn)); }
+        else { successful_database_save(); }
 
 mysqli_close($conn);
 ?>
