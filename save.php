248922c87ae559a02dcd37c1008b00eedc20e960
Justin Manley <manleyjster@gmail.com>
2014-05-20 13:22:39 -0500

    Fix spatialsurvey.Timestamp() object.  Add SurveyResponse.reset() method.

--- save.php
+++ save.php
@@ -11,15 +11,8 @@ if ( !$conn ) { die ('Could not connect: ' . mysqli_error($conn)); }
 
 $jsondata = mysqli_escape_string($conn, $_SESSION['path-data']);
 
-if ( $_SESSION['path-data'] == "{}")
-	$kml = "NOT ON CAMPUS";
-else {
-	$kmlstring = require_once('kml_template.php');
-	$kml = mysqli_escape_string($conn, $kmlstring);	
-}
-
 $sql = <<<sqlstring
-INSERT INTO paths ( id, kml_string, json_string, time_submitted ) VALUES ( DEFAULT, "$kml", "$jsondata", NOW() )
+INSERT INTO paths ( id, kml_string, json_string, time_submitted ) VALUES ( DEFAULT, "", "$jsondata", NOW() )
 sqlstring;
 
 mysqli_select_db($conn, $dbname);
